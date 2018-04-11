09482f6dd3be8e7b0e5965154143e144bfd2ac96
Tan Jia Jun <35268193+JiaJunTan@users.noreply.github.com>
2018-04-12 01:07:52 +0800

    Update NBA+Team+Season+Statistics+Scrapper.py

--- NBA+Team+Season+Statistics+Scrapper.py
+++ NBA+Team+Season+Statistics+Scrapper.py
@@ -1,7 +1,3 @@
-
-# coding: utf-8
-
-# In[84]:
 from selenium import webdriver
 import pandas as pd
 import numpy as np
@@ -9,28 +5,10 @@ import time
 import re
 from collections import OrderedDict
 
-
-# In[85]:
-
-# start_year = str(2016)
-# final_start = str(2000)
-# final_end = str(int((final_start)[2:]) + 1)
-# season = "{}-{}".format(start_year,end_year.zfill(2))
-# final_season = "{}-{}".format(final_start,final_end.zfill(2))
-
-
-# In[86]:
-
 driver = webdriver.Chrome(executable_path="/Users/JiaJun/Downloads/chromedriver")
-
-
-# In[87]:
 
 filepath = "https://stats.nba.com/teams/boxscores-traditional/?Season=2016-17&SeasonType=Regular%20Season"
 driver.get(filepath)
-
-
-# In[76]:
 
 def func(plyr_copy):
     file2 = []
@@ -47,29 +25,17 @@ def func(plyr_copy):
     print(season)
     return dic
 
-
-# In[71]:
-
 plyr = driver.find_elements_by_tag_name('tr')
-
-
-# In[72]:
 
 file = []
 for row in plyr:
     file.append(row.text)
 file = file[:31]
 
-
-# In[73]:
-
 dic = OrderedDict()
 for i in file[0].split(" "):
     dic[i] = []
 dic['Season'] = []
-
-
-# In[78]:
 
 button = driver.find_element_by_xpath(r"//div[@class='stats-table-pagination__inner stats-table-pagination__inner--top']//div[@class='stats-table-pagination__info']//a[@class='stats-table-pagination__next']")
 while True:
@@ -78,13 +44,6 @@ while True:
     button.click()
     time.sleep(3)
 
-
-# In[80]:
-
 df = pd.DataFrame(dic)
 
-
-# In[81]:
-
 df.to_csv(r"/Users/JiaJun/Desktop/Jupyter Notebook/nba-teams.csv",index=None)
-
