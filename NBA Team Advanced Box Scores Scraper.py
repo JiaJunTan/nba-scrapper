c46adf9d59c38ff4f2018403b6adf64587e492f4
Tan Jia Jun <35268193+jiajuntan@users.noreply.github.com>
2018-04-16 13:31:45 +0800

    Change of parser from Selenium to BeautifulSoup
    Initial Wall time w/ Selenium: 9min 21s
    Improved Wall Time w/ BeautifulSoup: 1min 20s

--- NBA Team Advanced Box Scores Scraper.py
+++ NBA Team Advanced Box Scores Scraper.py
@@ -1,53 +1,59 @@
+# Importing required libraries
 from selenium import webdriver
+from bs4 import BeautifulSoup as bsoup
+from collections import OrderedDict
 import pandas as pd
 import numpy as np
-import time
-import re
-from collections import OrderedDict
 
-driver = webdriver.Chrome(executable_path="/Users/JiaJun/Downloads/chromedriver")
-
+# Defining season of interest to scrap
 start_year = str(2016)
 end_year = str(int(start_year)+1)[2:].zfill(2)
 season = "{}-{}".format(start_year,end_year)
 
+# Selenium's WebDriver for webpage automation and BeautifulSoup as parser
+driver = webdriver.Chrome(executable_path="/Users/JiaJun/Downloads/chromedriver")
 filepath = "https://stats.nba.com/teams/boxscores-traditional/?Season={}&SeasonType=Regular%20Season".format(season)
 driver.get(filepath)
+bs_obj = bsoup(driver.page_source, 'html.parser')
+rows = bs_obj.find_all('table')[0].find_all('tr')
 
-def func(plyr_copy):
-    file2 = []
-    for row in plyr:
-        file2.append(row.text)
-        file2 = file2[:31]
-    dic['TEAM'].extend(list(map(lambda x: x.split("\n")[1] ,file2[1:])))
-    remaining_data = list(np.transpose(list(map(lambda x: x.split("\n")[2].split(" ") ,file2[1:]))))
-    i = 0
-    for col in list(dic.keys())[1:-1]:
-        dic[col].extend(remaining_data[i])
-        i += 1
-    dic['Season'].extend([season]*30)
-    print(season)
-    return dic
-
-plyr = driver.find_elements_by_tag_name('tr')
-
+# OrderedDictionary to store scrapped data and defining keys as DataFrame columns
 file = []
-for row in plyr:
-    file.append(row.text)
-file = file[:31]
+for row in rows:
+    file.append(row.get_text().strip().split("\n"))
 
 dic = OrderedDict()
-for i in file[0].split(" "):
-    dic[i] = []
-dic['Season'] = []
+for i in file[0]:
+    if i == 'Match\xa0Up':
+        dic['Match'] = []
+    elif i == 'Game\xa0Date':
+        dic['Date'] = []
+    else:
+        dic[i] = []
+        
+# Main function in loop to process and store data
+def func(rows):
+    file2 = []
+    for row in rows:
+        file2.append(row.get_text().strip().split("\n"))
+    file2 = np.transpose(file2[1:])
+    i = 0
+    for key in dic.keys():
+        dic[key].extend(list(file2[i]))
+        i += 1
+    return dic
 
+# Automation through WebDriver and looping through all pages of data 
 button = driver.find_element_by_xpath(r"//div[@class='stats-table-pagination__inner stats-table-pagination__inner--top']//div[@class='stats-table-pagination__info']//a[@class='stats-table-pagination__next']")
-while True:
-    plyr = driver.find_elements_by_tag_name('tr')
-    dic = func(plyr)
+page = 1
+while page <= 50:
+    bs_obj = bsoup(driver.page_source, 'html.parser')
+    rows = bs_obj.find_all('table')[0].find_all('tr')
+    dic = func(rows)
     button.click()
-    time.sleep(3)
+    page += 1
 
+# Transforming into DataFrame and exporting as csv
 df = pd.DataFrame(dic)
-
-df.to_csv(r"/Users/JiaJun/Desktop/Jupyter Notebook/nba-teams.csv",index=None)
+filedestination = r"/Users/JiaJun/Desktop/nba-teams_{}.csv".format(season)
+df.to_csv(filedestination,index=None)
